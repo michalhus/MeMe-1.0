bb16f5c5807a6edb67b5d1dc22e67050678a0863
Michal Hus <michalhus@WTA-MBP466.local>
2020-02-03 12:13:43 -0500

    Initial Commit

---
+++ Image Pick/ViewController.swift
@@ -0,0 +1,144 @@
+//
+//  ViewController.swift
+//  Image Pick
+//
+//  Created by Michal Hus on 1/31/20.
+//  Copyright © 2020 Michal Hus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var top: UITextField!
+    @IBOutlet weak var bottom: UITextField!
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: 2.0,
+    ]
+    
+    
+    
+
+//    When a user taps inside a textfield, the default text should clear.
+//    This can be accomplished in textFieldDidBeginEditing method.
+//    Be sure to remove default text only, not user entered text.
+
+    
+    
+//  When a user presses return, the keyboard should be dismissed.
+//  This can be accomplished in textFieldShouldReturn.
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        top.text = "TOP"
+//        top.placeholder = "TOP"
+        bottom.text = "BOTTOM"
+//        bottom.placeholder = "BOTTOM"
+        top.textAlignment = .center
+        bottom.textAlignment = .center
+        top.defaultTextAttributes = memeTextAttributes
+        bottom.defaultTextAttributes = memeTextAttributes
+        top.delegate = self
+        bottom.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+//    Review This code
+//    didFinishPickingMediaWithInfo is part of the documentation need to review it
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        if(textField == self.top && top.text == "TOP"){
+            top.text = ""
+        }else if (textField == self.bottom && bottom.text == "BOTTOM"){
+            bottom.text = ""
+        }
+        return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    
+
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+
+
+
+    @objc func keyboardWillShow(_ notification:Notification) {
+        view.frame.origin.y = -getKeyboardHeight(notification)
+    }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = +getKeyboardHeight(notification)
+        
+//        Subscribe to the keyboard notification: .UIKeyboardWillHide
+//        Write the method keyboardWillHide to move the view frame back down to 0
+    }
+    
+    
+
+// Review info.plist
+
+}
+
