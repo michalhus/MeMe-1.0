e6e3ed9cc25f8d49483d5152e74f1e2cbdb5099c
Michal Hus <43967855+michalhus@users.noreply.github.com>
2020-02-05 15:59:01 -0500

    Fix per pr (#3)
    * Fixed TextField specs and contains.
    
    * fixing constrains
    
    * Fixed Info.plist
    Added Share and Cancel behaviors
    Fixed up all constrains
    Fixed keyboard hiding.
    
    * Changes per PR.
    Removed Test files/folder.
    Made code more abstract.
    
    * fixed per PR

---
+++ Meme 1.0/Meme.swift
@@ -0,0 +1,19 @@
+//
+//  Meme.swift
+//  Image Pick
+//
+//  Created by Michal Hus on 2/5/20.
+//  Copyright © 2020 Michal Hus. All rights reserved.
+//
+
+import UIKit
+
+extension CreateMemeVC {
+    
+    struct Meme {
+        var topText: String?
+        var bottomText: String?
+        var originalImage: UIImage?
+        var memedImage: UIImage
+    }
+}
