8ade1aa221e179073a68d398be83cefc23eecfe1
Michal Hus <michalhus@WTA-MBP466.local>
2020-02-05 14:11:41 -0500

    Changes per PR. Removed Test files/folder. Made code more abstract.

--- Image Pick/CreateMemeVC.swift
+++ Image Pick/CreateMemeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+class CreateMemeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var topTextField: UITextField!
@@ -19,37 +19,28 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     
-    let memeTextAttributes: [NSAttributedString.Key: Any] = [
-        NSAttributedString.Key.strokeColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedString.Key.strokeWidth: 2.0,
-    ]
-    
-    struct Meme {
-        var topText: String?
-        var bottomText: String?
-        var originalImage: UIImage?
-        var memedImage: UIImage
+    func memeTextAttribute() -> [NSAttributedString.Key : Any] {
+        let textStyle = NSMutableParagraphStyle()
+        textStyle.alignment = .center
+        
+        let memeTextAttributes : [NSAttributedString.Key : Any] = [
+            .strokeColor: UIColor.black,
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            .strokeWidth: -4.0,
+            NSAttributedString.Key.paragraphStyle: textStyle
+        ]
+        
+        return memeTextAttributes
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         enableShareToggle()
-
-        topTextField.sizeToFit()
-        topTextField.placeholder = "TOP"
-        topTextField.textAlignment = .center
-        topTextField.defaultTextAttributes = memeTextAttributes
-        topTextField.delegate = self
-
-        bottomTextField.sizeToFit()
-        bottomTextField.placeholder = "BOTTOM"
-        bottomTextField.textAlignment = .center
-        bottomTextField.defaultTextAttributes = memeTextAttributes
-        bottomTextField.delegate = self
+        setupTextFieldStyle(textField: topTextField)
+        setupTextFieldStyle(textField: bottomTextField)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
@@ -61,26 +52,56 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         unsubscribeFromKeyboardNotifications()
         super.viewWillDisappear(animated)
     }
-
+    
     @IBAction func resetMeMe(_ sender: Any) {
         imageView.image = nil
         topTextField.text = ""
         bottomTextField.text = ""
+        enableShareToggle()
     }
     
     @IBAction func pickAnImageFromAlbum(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        pickerController.delegate = self
-        pickerController.sourceType = .photoLibrary
-        present(pickerController, animated: true, completion: nil)
+        openImagePicker(.photoLibrary)
     }
     
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .camera
-
-        present(imagePicker, animated: true, completion: nil)
+        openImagePicker(.camera)
+    }
+    
+    @IBAction func memeShare(_ sender: Any) {
+        //generate a memed image
+        let memeToShare = [ generateMemedImage() ]
+        
+        //define an instance of the ActivityViewController
+        //pass the ActivityViewController a memedImage as an activity item
+        let activityViewController = UIActivityViewController(activityItems: memeToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        
+        //present the ActivityViewController
+        self.present(activityViewController, animated: true, completion: nil)
+        
+        activityViewController.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) -> Void in
+            if completed {
+                self.save()
+            }else {
+                return
+            }
+        }
+    }
+    
+    func setupTextFieldStyle(textField: UITextField) {
+        textField.sizeToFit()
+        textField.placeholder = "Meme Text"
+        textField.defaultTextAttributes = memeTextAttribute()
+        textField.autocapitalizationType = .allCharacters
+        textField.delegate = self
+    }
+    
+    func openImagePicker(_ source: UIImagePickerController.SourceType){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = source
+        present(picker, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -93,7 +114,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         return true
     }
-
+    
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
         return true
     }
@@ -107,18 +128,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
     func unsubscribeFromKeyboardNotifications() {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
     @objc func keyboardWillShow(_ notification:Notification) {
         if bottomTextField.isEditing{
             view.frame.origin.y = -getKeyboardHeight(notification)
         }
     }
-
+    
     @objc func keyboardWillHide(_ notification:Notification) {
         view.frame.origin.y = 0.0
     }
@@ -131,19 +152,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func generateMemedImage() -> UIImage {
         
-        // Hide toolbar and navbar
-        self.toolbar.isHidden = true
-        self.navigationBar.isHidden = true
+        hideToolbars(true)
         
         // Render view to an image
-    UIGraphicsBeginImageContext(self.view.frame.size)
+        UIGraphicsBeginImageContext(self.view.frame.size)
         view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
         let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         
-        // Hide toolbar and navbar
-        self.toolbar.isHidden = false
-        self.navigationBar.isHidden = false
+        hideToolbars(false)
         
         return memedImage
     }
@@ -162,25 +179,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    @IBAction func memeShare(_ sender: Any) {
-        //generate a memed image
-        let memeToShare = [ generateMemedImage() ]
-
-        //define an instance of the ActivityViewController
-        //pass the ActivityViewController a memedImage as an activity item
-        let activityViewController = UIActivityViewController(activityItems: memeToShare, applicationActivities: nil)
-        activityViewController.popoverPresentationController?.sourceView = self.view
-
-        //present the ActivityViewController
-        self.present(activityViewController, animated: true, completion: nil)
-
-        activityViewController.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) -> Void in
-            if completed == true {
-                self.save()
-            }else {
-               return
-            }
-        }
+    func hideToolbars(_ hide: Bool) {
+        self.toolbar.isHidden = hide
+        self.navigationBar.isHidden = hide
     }
+    
 }
 
