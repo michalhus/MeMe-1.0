4eff02ea5a9f9255263a08688b0271a7e03d14b5
Michal Hus <43967855+michalhus@users.noreply.github.com>
2020-02-10 15:29:35 -0500

    WIP - added some code from villains project to test out TableView concepts.

---
+++ Meme 1.0/SentMemesCollectionVC.swift
@@ -0,0 +1,12 @@
+//
+//  SentMemesCollectionVC.swift
+//  Meme 1.0
+//
+//  Created by Michal Hus on 2/10/20.
+//  Copyright © 2020 Michal Hus. All rights reserved.
+//
+
+import Foundation
+
+//let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//memes = appDelegate.memes
