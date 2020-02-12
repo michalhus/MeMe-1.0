2e593a47a14d793ecc0402149e42e8f26d7284f2
Michal Hus <43967855+michalhus@users.noreply.github.com>
2020-02-12 10:26:30 -0500

    Fixed reference an connections for Collection View

---
+++ Meme 1.0/MemeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCollectionViewCell.swift
+//  Meme 1.0
+//
+//  Created by Michal Hus on 2/11/20.
+//  Copyright © 2020 Michal Hus. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var memeLabel: UILabel!
+}
--- Meme 1.0/SentMemesCollectionVC.swift
+++ Meme 1.0/SentMemesCollectionVC.swift
@@ -7,6 +7,50 @@
 //
 
 import Foundation
+import UIKit
 
-//let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//memes = appDelegate.memes
+class SentMemesCollectionVC:  UICollectionViewController {
+    
+    // MARK: Properties
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
+    // MARK: Life Cycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+        collectionView!.reloadData()
+    }
+    
+    // MARK: Collection View Data Source
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! MemeCollectionViewCell
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        // Set the name and image
+        cell.memeLabel.text = meme.bottomText
+        cell.memeImageView?.image = meme.memedImage
+        //cell.schemeLabel.text = "Scheme: \(villain.evilScheme)"
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "MemeDetailVC") as! MemeDetailViewController
+        detailController.meme = self.memes[(indexPath as NSIndexPath).row]
+        self.navigationController!.pushViewController(detailController, animated: true)
+        
+    }
+    
+    
+}
